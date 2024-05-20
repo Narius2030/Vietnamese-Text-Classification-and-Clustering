f2cfd9470447ff42f22b1dbf05ea84801fa9daa8
Narius2030 <buiducnhan15122003@gmail.com>
2024-05-20 17:11:23 +0700

    feat: complete text clustering with Kmean task

--- src/dtprocess/cleandt.py
+++ src/dtprocess/cleandt.py
@@ -1,4 +1,6 @@
 import ast
+import re
+import string
 
 def stopwords_vi(stopword_path) -> list:
     # './src/vietnamese-stopwords.txt'
@@ -22,3 +24,34 @@ def convert_dict(news:list, content) -> list:
         lst.append(new_dict)
 
     return lst
+
+def remove_punctuation(comment):
+    # Create a translation table
+    translator = str.maketrans('', '', string.punctuation)
+    # Remove punctuation
+    new_string = comment.translate(translator)
+    # Remove redudant space and break sign
+    new_string = re.sub('[\n ]+', ' ', new_string)
+    # Remove emoji icon
+    emoji_pattern = re.compile("["
+        u"\U0001F600-\U0001F64F"  # emoticons
+        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+        u"\U00002500-\U00002BEF"  # chinese char
+        u"\U00002702-\U000027B0"
+        u"\U000024C2-\U0001F251"
+        u"\U0001f926-\U0001f937"
+        u"\U00010000-\U0010ffff"
+        u"\u2640-\u2642"
+        u"\u2600-\u2B55"
+        u"\u200d"
+        u"\u23cf"
+        u"\u23e9"
+        u"\u231a"
+        u"\ufe0f"  # dingbats
+        u"\u3030"
+                            "]+", flags=re.UNICODE)
+    new_string = re.sub(emoji_pattern, '', new_string)
+
+    return new_string
